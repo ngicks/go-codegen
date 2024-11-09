63c1f0830ac588e1330271150ced35e646132158
ngicks <yknt.bsl@gmail.com>
2024-11-09 01:07:12 +0000

    🚚: move import related

--- codegen/undgen/imports.go
+++ codegen/undgen/imports.go
@@ -16,6 +16,25 @@ import (
 	"github.com/ngicks/go-iterator-helper/x/exp/xiter"
 )
 
+type TargetImport struct {
+	ImportPath string
+	Types      []string
+}
+
+func AppendTargetImports(tis []TargetImport, additive TargetImport) []TargetImport {
+	idx := slices.IndexFunc(tis, func(t TargetImport) bool { return t.ImportPath == additive.ImportPath })
+	if idx >= 0 {
+		t := tis[idx]
+		t.Types = append(t.Types, additive.Types...)
+		slices.Sort(t.Types)
+		t.Types = slices.Compact(t.Types)
+		tis[idx] = t
+	} else {
+		tis = append(tis, additive)
+	}
+	return tis
+}
+
 type TargetType struct {
 	ImportPath string
 	Name       string
