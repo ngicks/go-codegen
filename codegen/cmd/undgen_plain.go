1158d186328f509144b21245d9612969e64ae419
ngicks <yknt.bsl@gmail.com>
2024-12-08 07:43:43 +0000

    🧹: use runCommand helper
    It also no longer removes generated files before generation.
    Since it would prevent the type checker from parsing types in the wild.
    Users might define code depending generated code in the packages that are being parsed to generate code.
    
    It also fix incorrect sub command name in CODE GENERATED BY header comment.

--- codegen/cmd/undgen_plain.go
+++ codegen/cmd/undgen_plain.go
@@ -4,65 +4,35 @@ Copyright © 2024 ngicks <yknt.bsl@gmail.com>
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/ngicks/go-codegen/codegen/generator/undgen"
-	"github.com/ngicks/go-codegen/codegen/pkgsutil"
-	"github.com/ngicks/go-iterator-helper/hiter"
+	"github.com/ngicks/go-codegen/codegen/suffixwriter"
 	"github.com/spf13/cobra"
+	"golang.org/x/tools/go/packages"
 )
+
+func init() {
+	fset := undgenPlainCmd.Flags()
+	commonFlags(fset, true)
+	undgenCmd.AddCommand(undgenPlainCmd)
+}
 
 // undgenPlainCmd represents the patch command
 var undgenPlainCmd = &cobra.Command{
 	Use:   "plain",
 	Short: "undgen-plain generates plain types and conversion methods from target types.",
 	Long:  `undgen-plain generates plain types and conversion methods from target types.`, // TODO improve
-	RunE: func(cmd *cobra.Command, args []string) error {
-		fset := cmd.Flags()
-
-		dir, pkg, verbose, ignoreGenerated, dry, err := commonOpts(fset, true)
-		if err != nil {
-			return err
-		}
-		if verbose {
-			fmt.Printf("running: undgen plain\n\n\n")
-		}
-		ctx := cmd.Context()
-
-		if verbose {
-			fmt.Printf("loading: %#v\n", pkg)
-		}
-		targetPkgs, err := loadPkgs(ctx, dir, pkg, true, verbose, ignoreGenerated)
-		if err != nil {
-			return err
-		}
-
-		const suffix = ".und_plain"
-		writer, deferred := createWriter(dir, suffix, "plain", verbose, dry)
-		defer deferred()
-
-		err = hiter.TryForEach(
-			func(s string) {
-				if verbose || dry {
-					if dry {
-						fmt.Printf("\t[DRY]: removed %q\n", s)
-					} else {
-						fmt.Printf("\tremoved %q\n", s)
-					}
-				}
-			},
-			pkgsutil.RemoveSuffixedFiles(targetPkgs, dir, suffix, dry),
-		)
-		if err != nil {
-			return err
-		}
-
-		return undgen.GeneratePlain(writer, verbose, targetPkgs, undgen.ConstUnd.Imports)
-	},
-}
-
-func init() {
-	fset := undgenPlainCmd.Flags()
-	commonFlags(fset, true)
-	undgenCmd.AddCommand(undgenPlainCmd)
+	RunE: runCommand(
+		"undgen plain",
+		".und_plain",
+		true,
+		func(
+			cmd *cobra.Command,
+			writer *suffixwriter.Writer,
+			verbose bool,
+			pkgs []*packages.Package,
+			args []string,
+		) error {
+			return undgen.GeneratePlain(writer, verbose, pkgs, undgen.ConstUnd.Imports)
+		},
+	),
 }
